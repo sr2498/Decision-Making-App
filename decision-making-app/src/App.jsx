c7264070b676fe86bc3cd6643a04db036a6a7453
Sandhya Rani <sr2498@njit.edu>
2025-05-08 19:26:41 -0400

    modified app.jsx

--- decision-making-app/src/App.jsx
+++ decision-making-app/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,6 +6,7 @@ function App() {
     <>
       <div>
              <h1>Decision Making App</h1>
+             <h2></h2>
           </div>
        </>
   )
